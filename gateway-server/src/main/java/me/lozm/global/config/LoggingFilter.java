74649dd00bca264540ac7a3f4d9708320bc2d999
이준 <laonzenamoon@gmail.com>
2021-11-24 14:19:34 +0900

    feat: gateway-server > LoggingFilter 기능 개발

---
+++ gateway-server/src/main/java/me/lozm/global/config/LoggingFilter.java
@@ -0,0 +1,68 @@
+package me.lozm.global.config;
+
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.OrderedGatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.core.Ordered;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+@Slf4j
+@Component
+public class LoggingFilter extends AbstractGatewayFilterFactory<LoggingFilter.Config> {
+
+    @Data
+    public static class Config {
+        private String baseMessage;
+        private boolean preLogger;
+        private boolean postLogger;
+    }
+
+    public LoggingFilter() {
+        super(Config.class);
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+//        return (exchange, chain) -> {
+//            final ServerHttpRequest request = exchange.getRequest();
+//            final ServerHttpResponse response = exchange.getResponse();
+//
+//            log.info("Logging filter baseMessage: {}", config.getBaseMessage());
+//            if (config.isPreLogger()) {
+//                log.info("Logging PRE filter: request id -> {}", request.getId());
+//            }
+//
+//            return chain.filter(exchange)
+//                    .then(Mono.fromRunnable(() -> {
+//                        if (config.isPostLogger()) {
+//                            log.info("Logging POST filter: response code -> {}", response.getStatusCode());
+//                        }
+//                    }));
+//        };
+
+        final GatewayFilter orderedGatewayFilter = new OrderedGatewayFilter(((exchange, chain) -> {
+            final ServerHttpRequest request = exchange.getRequest();
+            final ServerHttpResponse response = exchange.getResponse();
+
+            log.info("Logging filter baseMessage: {}", config.getBaseMessage());
+            if (config.isPreLogger()) {
+                log.info("Logging PRE filter: request id -> {}", request.getId());
+            }
+
+            return chain.filter(exchange)
+                    .then(Mono.fromRunnable(() -> {
+                        if (config.isPostLogger()) {
+                            log.info("Logging POST filter: response code -> {}", response.getStatusCode());
+                        }
+                    }));
+        }), Ordered.HIGHEST_PRECEDENCE);
+
+        return orderedGatewayFilter;
+    }
+
+}
