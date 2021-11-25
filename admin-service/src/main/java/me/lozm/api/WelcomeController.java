abfbdfd9019bb8f360046ebb87bfcd565f23df15
이준 <laonzenamoon@gmail.com>
2021-11-25 11:27:24 +0900

    feat: 마이크로서비스 port 확인 API 추가

--- admin-service/src/main/java/me/lozm/api/WelcomeController.java
+++ admin-service/src/main/java/me/lozm/api/WelcomeController.java
@@ -1,13 +1,23 @@
 package me.lozm.api;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
+@Slf4j
 @RequestMapping("admin-service")
 @RestController
+@RequiredArgsConstructor
 public class WelcomeController {
+
+    private final Environment environment;
+
 
     @GetMapping("welcome")
     public String welcomeAdminService() {
@@ -16,8 +26,14 @@ public class WelcomeController {
 
     @GetMapping("header-test")
     public String testHeader(@RequestHeader("admin-request") String requestHeader) {
-        System.out.println("requestHeader = " + requestHeader);
+        log.info("requestHeader = " + requestHeader);
         return "Welcome to Admin Service";
+    }
+
+    @GetMapping("check")
+    public String check(HttpServletRequest httpServletRequest) {
+        log.info("Server port = {}", httpServletRequest.getServerPort());
+        return String.format("Admin Service Information: Port(%s)", environment.getProperty("local.server.port"));
     }
 
 }
--- user-service/src/main/java/me/lozm/api/WelcomeController.java
+++ user-service/src/main/java/me/lozm/api/WelcomeController.java
@@ -1,13 +1,23 @@
 package me.lozm.api;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
+@Slf4j
 @RequestMapping("user-service")
 @RestController
+@RequiredArgsConstructor
 public class WelcomeController {
+
+    private final Environment environment;
+
 
     @GetMapping("welcome")
     public String welcomeUserService() {
@@ -16,8 +26,14 @@ public class WelcomeController {
 
     @GetMapping("header-test")
     public String testHeader(@RequestHeader("user-request") String requestHeader) {
-        System.out.println("requestHeader = " + requestHeader);
+        log.info("requestHeader = " + requestHeader);
         return "Welcome to User Service";
+    }
+
+    @GetMapping("check")
+    public String check(HttpServletRequest httpServletRequest) {
+        log.info("Server port = {}", httpServletRequest.getServerPort());
+        return String.format("User Service Information: Port(%s)", environment.getProperty("local.server.port"));
     }
 
 }
